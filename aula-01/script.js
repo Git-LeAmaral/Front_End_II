4e1d179c5abff4003aa6859ec05c11f2bb2723fa
Leandro Amaral <lramaral02m@gmail.com>
2023-02-14 12:42:59 -0300

    aula 01 front end II

---
+++ aula-01/script.js
@@ -0,0 +1,18 @@
+// Declarando uma variavel
+// var userName = 'Leandro';
+// let idade = 36;
+// const job = 'Tech';
+
+// console.log(userName);
+// window.prompt('Bem vindo!')
+
+// var userName;
+
+// console.log( window.prompt('Bem vindo!') );
+
+var userName = window.prompt('Bem vindo!');
+const welcomeMessage = 'Olá' + userName
+
+console.log(welcomeMessage);
+
+alert(welcomeMessage);
